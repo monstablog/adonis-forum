b9e235f2254de490ef6b435c6f3d06da89c148b5
Henry Oladeji <officialmonstajamss@gmail.com>
2020-07-02 19:38:43 +0100

    Minor Update

--- start/hooks.js
+++ start/hooks.js
@@ -2,13 +2,18 @@
 
 const { hooks } = require('@adonisjs/ignitor')
 
-hooks.after.providersBooted(()=> {
+hooks.after.providersBooted(async ()=> {
     const View = use('View')
     const Env = use('Env')
+    const { DateTime } = require('luxon')
 
     View.global('appUrl', path => {
         const APP_URL = Env.get('APP_URL')
 
         return path ? `${APP_URL}/${path}` : APP_URL
     })
+
+    View.global('todayDate', () => {
+        return DateTime.fromJSDate(new Date()).toFormat('LLLL\',\' d cccc y \'at\' h:m a')
+      })
 })
